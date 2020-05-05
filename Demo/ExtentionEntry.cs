0a953964d58f023c6f7a700c35c6520ba1752983
Leonard Cooke <leonardlcooke@hotmail.com>
2020-05-05 15:12:06 -0600

    Added ExampleRedirectMerchant. Removed GetMerchantHook.

--- Demo/ExtentionEntry.cs
+++ Demo/ExtentionEntry.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using DirectScale.Disco.Extension;
-using DirectScale.Disco.Extension.Hooks.Merchants.ExtendedMerchants;
 using DirectScale.Disco.Extension.Hooks;
 using DirectScale.Disco.Extension.Api;
-using Demo.Hooks;
 using Demo.Views;
 using Demo.Api;
+using Demo.Merchants;
 
 namespace Demo
 {
@@ -14,14 +13,14 @@ namespace Demo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IDemoService, DemoService>();
-            services.AddSingleton<IHook<GetExtendedMerchantsHookRequest, GetExtendedMerchantsHookResponse>, GetMerchantHook>();
+
+
 
             services.AddSingleton<IApiEndpoint, Endpoint1>();
             services.AddSingleton<IApiEndpoint, Endpoint2>();
             services.AddSingleton<IApiEndpoint, Endpoint3>();
 
-            services.AddSingleton<IViewEndpoint, View1>();
-            services.AddSingleton<IViewEndpoint, View2>();
+            services.AddScoped<IMoneyInMerchant, ExampleRedirectMerchant>();
         }
     }
 }
---
+++ Demo/Merchants/ExampleRedirectMerchant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DirectScale.Disco.Extension;
+
+namespace Demo.Merchants
+{
+    public class ExampleRedirectMerchant : RedirectMoneyInMerchant
+    {
+        public ExampleRedirectMerchant() : base(
+            new MerchantInfo
+            {
+                Currency = "USD",
+                DisplayName = "ExampleDemo",
+                Id = 9933,
+                MerchantName = "Example"
+            }
+        ){}
+
+        public override PaymentResponse ChargePayment(int associateId, int orderNumber, Address billingAddress, double amount, string currencyCode, string redirectUrl)
+        {
+            throw new Exception("Unable to Charge Payment");
+        }
+
+        public override PaymentResponse RefundPayment(string payerId, int orderNumber, string currencyCode, double paymentAmount, double refundAmount, string referenceNumber, string transactionNumber, string authorizationCode)
+        {
+            throw new Exception("Unable to Refund Payment");
+        }
+    }
+}
