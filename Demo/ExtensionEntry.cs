788f8220cf7e7e2351a4faf361ffaf5e295763e9
Dillon Swanson <dillon@directscale.com>
2020-09-08 13:21:38 -0600

    example of client setup

--- Demo/ExtensionEntry.cs
+++ Demo/ExtensionEntry.cs
@@ -2,13 +2,12 @@
 using DirectScale.Disco.Extension;
 using DirectScale.Disco.Extension.Hooks;
 using DirectScale.Disco.Extension.Api;
-using Demo.Views;
 using Demo.Api;
 using Demo.Merchants;
-using System.Data;
-using System.Data.SqlClient;
+using DirectScale.Disco.Extension.Hooks.Orders;
+using System;
+using FlexPay;
 using DirectScale.Disco.Extension.Services;
-using MobileCoach;
 
 namespace Demo
 {
@@ -19,22 +18,97 @@ namespace Demo
             services.AddSingleton<IDemoService, DemoService>();
 
             services.AddSingleton<IApiEndpoint, Endpoint1>();
-            services.AddSingleton<IApiEndpoint, Endpoint2>();
-            services.AddSingleton<IApiEndpoint, Endpoint3>();
-            
+            //services.AddSingleton<IApiEndpoint, Endpoint2>();
+            //services.AddSingleton<IApiEndpoint, Endpoint3>();
+                
             services.AddScoped<IMoneyInMerchant, ExampleRedirectMerchant>();
 
-            services.UseMobileCoach(new MobileCoachDirectScale.Config
-            {
-                Token = "8Nzegi_iIgVgwHRiOOZD9_oGp13LkXueLxiS",
-                SecretKey = "mWloZHnf6SOXot1rbPcTrAlGJXAdHTupV7LI",
-                UrlKey = "134a9bac6f4532fe75399b2371377313e780698797b2dc4ba72fbe425da27db4",
-                BaseUrl = "https://qa.mobilecoach.com/widgets/" //"https://admin.mobilecoach.com/widgets/"
-            });
+            services.AddTransient<IHook<RefundPaymentHookRequest, RefundPaymentHookResponse>, RefundPayment>();
+            services.AddTransient<IHook<FinalizeNonAcceptedOrderHookRequest, FinalizeNonAcceptedOrderHookResponse>, FinalizeNonAcceptedOrder>();
+            services.AddTransient<IHook<FinalizeAcceptedOrderHookRequest, FinalizeAcceptedOrderHookResponse>, FinalizeAcceptedOrder>();
+            services.UseFlexPay();
 
             // Transient: Create a new one every time.
             // Singleton: Once in life of service. Cleared when IIS restarts.
             // Scoped: Once per HTTPContext request
+        }
+
+        public class FinalizeNonAcceptedOrder : IHook<FinalizeNonAcceptedOrderHookRequest, FinalizeNonAcceptedOrderHookResponse>
+        {
+            private readonly IFlexPayService _flexPayService;
+            private readonly IOrderService _orderService;
+
+            public FinalizeNonAcceptedOrder(IFlexPayService flexPayService, IOrderService orderService)
+            {
+                _flexPayService = flexPayService;
+                _orderService = orderService;
+            }
+
+            public FinalizeNonAcceptedOrderHookResponse Invoke(FinalizeNonAcceptedOrderHookRequest request, Func<FinalizeNonAcceptedOrderHookRequest, FinalizeNonAcceptedOrderHookResponse> func)
+            {
+                try
+                {
+                    _flexPayService.GetRecommendation(request.Order, new FlexPay.Models.BillingInfo
+                        {
+                            BillingAddress = new FlexPay.Models.Address
+                            {
+                                Address1 = "123 Test St",
+                                City = "Orem",
+                                State = "UT",
+                                Country = "US",
+                                PostalCode = "84058"
+                            },
+                            FirstSixDigits = "123456",
+                            LastFourDigits = "1111",
+                            GatewayType = FlexPay.Models.GatewayType.nmi,
+                            GatewayCode = "TG",
+                            GatewayMessage = "This is a test"
+                        }
+                    );
+                }
+                catch (Exception e)
+                {
+                    _orderService.Log(request.Order.OrderNumber, $"Demo - {e.Message}");
+                }
+
+                return func.Invoke(request);
+            }
+        }
+
+        public class FinalizeAcceptedOrder : IHook<FinalizeAcceptedOrderHookRequest, FinalizeAcceptedOrderHookResponse>
+        {
+            private readonly IFlexPayService _flexPayService;
+
+            public FinalizeAcceptedOrder(IFlexPayService flexPayService)
+            {
+                _flexPayService = flexPayService;
+            }
+
+            public FinalizeAcceptedOrderHookResponse Invoke(FinalizeAcceptedOrderHookRequest request, Func<FinalizeAcceptedOrderHookRequest, FinalizeAcceptedOrderHookResponse> func)
+            {
+                _flexPayService.NotifyOfOrderSuccess(request.Order);
+
+                return func.Invoke(request);
+            }
+        }
+
+        public class RefundPayment : IHook<RefundPaymentHookRequest, RefundPaymentHookResponse>
+        {
+            private readonly IFlexPayService _flexPayService;
+
+            public RefundPayment(IFlexPayService flexPayService)
+            {
+                _flexPayService = flexPayService;
+            }
+
+            public RefundPaymentHookResponse Invoke(RefundPaymentHookRequest request, Func<RefundPaymentHookRequest, RefundPaymentHookResponse> func)
+            {
+                var result = func.Invoke(request);
+
+                _flexPayService.NotifyOfRefund(request.OrderNumber);
+
+                return result;
+            }
         }
     }
 }
