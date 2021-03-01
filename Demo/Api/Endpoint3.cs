f32b63a23c6a4c4fa22fb084a73b85086fbd0ff0
Leonard Cooke <leonard@directscale.com>
2021-03-01 11:11:48 -0700

    Updated Endpoint3 to return stats.

--- Demo/Api/Endpoint3.cs
+++ Demo/Api/Endpoint3.cs
@@ -1,27 +1,20 @@
 ﻿using DirectScale.Disco.Extension.Api;
+using DirectScale.Disco.Extension.Services;
 
 namespace Demo.Api
 {
     public class Endpoint3 : ApiEndpoint<Endpoint3Request>
     {
-        public Endpoint3(IRequestParsingService parsingService) : base("demo/end3", parsingService)
-        {
+        private readonly IStatsService _statsService;
 
+        public Endpoint3(IRequestParsingService parsingService, IStatsService statsService) : base("demo/end3", parsingService)
+        {
+            _statsService = statsService;
         }
 
         public override IApiResponse Post(Endpoint3Request request)
         {
-            var clientId = System.Environment.GetEnvironmentVariable("client");
-
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            var resourceName = "Demo.Html.htmlpage.html";
-            using (System.IO.Stream stream = assembly.GetManifestResourceStream(resourceName))
-            using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
-            {
-                string data = reader.ReadToEnd();
-
-                return new Ok(new { data });
-            }
+            return new Ok(_statsService.GetStats(new[] { 2 }, System.DateTime.Now));
         }
     }
 }
